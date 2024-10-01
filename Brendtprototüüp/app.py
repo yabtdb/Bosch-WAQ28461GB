9a36f1fd2e7d122ac976490b305c0dea453a73d8
yabtdb <97367674+yabtdb@users.noreply.github.com>
2024-10-01 12:45:26 +0300

    Update app.py

--- Brendtprototüüp/app.py
+++ Brendtprototüüp/app.py
@@ -3,8 +3,12 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('avaleht.html') # malli renderdamine
-@app.route('/joogid')
+@app.route('/joogid', methods=['GET', 'POST'])
 def joogid():
     return render_template('joogid.html')
+    selected_options = request.form.getlist('checkbox')
+    print(selected_options)
+    return f'Selected options: {", ".join(selected_options)}'
+
 if __name__ == '__main__':
     app.run()
