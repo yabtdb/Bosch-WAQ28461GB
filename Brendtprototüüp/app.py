11d2c8f4b8d4ad82901b7f1a7334ff1d9cf237a9
yabtdb <97367674+yabtdb@users.noreply.github.com>
2024-10-08 12:46:03 +0300

    Update app.py

--- Brendtprototüüp/app.py
+++ Brendtprototüüp/app.py
@@ -1,14 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('avaleht.html') # malli renderdamine
 @app.route('/joogid', methods=['GET', 'POST'])
 def joogid():
-    return render_template('joogid.html')
-    selected_options = request.form.getlist('checkbox')
-    print(selected_options)
-    return f'Selected options: {", ".join(selected_options)}'
-
-if __name__ == '__main__':
-    app.run()
+        poed = {}
+        valik = request.args.getlist('pood') or request.form.getlist('pood')
+        if len(valik) == 1 and ',' in valik[0]:
+            poed['pood'] = comma_separated_params_to_list(valik[0])
+        else:
+            poed['pood'] = valik
+        print(poed)
+        
+        
+        return render_template('joogid.html', poed2=valik)
+@app.route('/joogid', methods=['GET', 'POST'])
+def comma_separated_params_to_list(param):
+    result = []
+    for val in param.split(','):
+        if val:
+            result.append(val)
+    return result
