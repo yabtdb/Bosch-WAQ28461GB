4593cc85c14d97d5a50f9dfd9770f7adce4fd7a8
yabtdb <97367674+yabtdb@users.noreply.github.com>
2024-10-13 18:00:22 +0300

    Update app.py

--- Brendtprototüüp/app.py
+++ Brendtprototüüp/app.py
@@ -5,7 +5,15 @@ def index():
     return render_template('avaleht.html') # malli renderdamine
 @app.route('/joogid', methods=['GET', 'POST'])
 def joogid():
+    
         poed = {}
+        joogidC = ["vesi", "coca-cola", "jäätis"]
+        joogidR = ["limpa", "marinaad", "süütevedelik"]
+        joogidS = ["maasikad", "kolmteist", "vs"]
+        joogidP = ["pepsi", "leonardo", "plastiliin"]
+        tooted = []
+        
+        
         valik = request.args.getlist('pood') or request.form.getlist('pood')
         if len(valik) == 1 and ',' in valik[0]:
             poed['pood'] = comma_separated_params_to_list(valik[0])
@@ -13,8 +21,27 @@ def joogid():
             poed['pood'] = valik
         print(poed)
         
+        for l in range(len(valik)):
+            if valik[l] == "Coop":
+                for m in range(len(joogidC)):
+                    tooted.append(joogidC[m])
+                    
+            if valik[l] == "Rimi":
+                for m in range(len(joogidR)):
+                    tooted.append(joogidR[m])
+                    
+                    
+            if valik[l] == "Selver":
+                for m in range(len(joogidS)):
+                    tooted.append(joogidS[m])
+                    
+            
+            if valik[l] == "Prisma":
+                for m in range(len(joogidP)):
+                    tooted.append(joogidP[m])
+
+        return render_template('joogid.html', poed2=valik, tooted2=tooted)
         
-        return render_template('joogid.html', poed2=valik)
 @app.route('/joogid', methods=['GET', 'POST'])
 def comma_separated_params_to_list(param):
     result = []
